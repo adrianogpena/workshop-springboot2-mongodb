79682529f9040ba8eb11533c84dfa6f010cceb98
adrianogpena <adrianogpena@gmail.com>
2020-11-12 19:28:14 -0300

    Obtendo um usuário por id

--- src/main/java/com/adrianogpena/workshopmongo/services/UserService.java
+++ src/main/java/com/adrianogpena/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.adrianogpena.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.adrianogpena.workshopmongo.domain.User;
 import com.adrianogpena.workshopmongo.repository.UserRepository;
+import com.adrianogpena.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
